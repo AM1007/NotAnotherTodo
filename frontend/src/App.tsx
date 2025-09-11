dfe0bf5f7be9e98a6b7cb5b0c8709ecb0a485034
Andrew Motko <andrew.motko@gmail.com>
2025-09-11 06:20:21 +0300

    init: project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        
+      <h1>Not Another Todo. Because the World Needs One More Anyway!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
